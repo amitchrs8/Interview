369613b2f11a254e2c65e6ceed96c905019f84f5
amitchrs8 <amitchrs8@gmail.com>
2023-06-04 23:11:37 +0530

    Adding Structure

---
+++ src/Basics/Practice_01.java
@@ -0,0 +1,12 @@
+package Basics;
+
+import java.util.stream.IntStream;
+
+public class Practice_01 {
+
+    public static void main(String[] args) {
+        System.out.println("Testing the execution of Program!!");
+    }
+
+
+}
